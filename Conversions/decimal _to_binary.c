d4df6829fbd2f46d8bd3d92152d798ab152cc24e
adnankocak <adnankocak_68@hotmail.com>
2018-08-02 14:56:57 +0300

    Update decimal _to_binary.c

--- Conversions/decimal _to_binary.c
+++ Conversions/decimal _to_binary.c
@@ -1,66 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAXBITS 100
+int start(int number)
+{
+	int i;	
+	for(i = 31;i >= 0;i--)
+		if(((number >> i) & 0x1) == 1)
+			return i;
+		
+	return 0;		
+}
+
+void printBinary(int number)
+{
+	int i;
+	printf("\nthe number in binary is: ");
+	
+	for(i = start(number);i >= 0;i--)
+		printf("%d",((number >> i) & 0x1));
+}
 
 int main()
 {
-
-    // input of the user
-    int inputNumber;
-
-    // for the remainder
-    int re;
-
-    // contains the bits 0/1
-    int bits[MAXBITS];
-
-    // for the loops
-    int j;
-    int i=0;
-
-    printf("\t\tConverter decimal --> binary\n\n");
+	int inputNumber;
+	
+	printf("\t\t32 Bit Converter (decimal --> binary)\n\n");
 
     // reads a decimal number from the user.
-    printf("\nenter a positive integer number: ");
+    printf("\nenter a integer number: ");
     scanf("%d",&inputNumber);
 
-    // make sure the input number is a positive integer.
-    if (inputNumber < 0)
-    {
-        printf("only positive integers >= 0\n");
-        return 1;
-    }
-
-    // actual processing
-    while(inputNumber>0)
-    {
-
-        // computes the remainder by modulo 2
-        re = inputNumber % 2;
-
-        // computes the quotient of division by 2
-        inputNumber = inputNumber / 2;
-
-        bits[i] = re;
-        i++;
-
-    }
-
-    printf("\n the number in binary is: ");
-
-    // iterates backwards over all bits
-    for(j=i-1; j>=0; j--)
-    {
-        printf("%d",bits[j]);
-    }
-
-    // for the case the input number is 0
-    if (i == 0)
-    {
-        printf("0");
-    }
-
-    return 0;
+	
+    printBinary(inputNumber);
+    
+    fflush(stdin);
+    getchar();
+    
+	return 0;
 }
-
